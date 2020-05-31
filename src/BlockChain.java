11ae101bb9e1205a1200d3218d521ac666e457d7
redfords <joanapiovaroli@gmail.com>
2020-05-30 23:33:31 -0300

    Add StringUtil

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -1,13 +1,33 @@
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
 public class BlockChain {
+
+   public static ArrayList<Block> blockchain = new ArrayList<Block>();
+
    public static void main(String[] args) {
-      Block firstBlock = new Block("This is the first block", "0");
-      System.out.println("Hash for block 1 : " + firstBlock.hash);
+      blockchain.add(new Block("This is the first block", "0"));
+      blockchain.add(new Block("This is the second block", blockchain.get(blockchain.size()-1).hash));
+      blockchain.add(new Block("This is the third block", blockchain.get(blockchain.size()-1).hash));
 
-      Block secondBlock = new Block("This is the second block", firstBlock.hash);
-      System.out.println("Hash for block 2 :" + secondBlock.hash);
+      String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+      System.out.println(blockchainJson);
 
-      Block thirdBlock = new Block("This is the third block", secondBlock.hash);
-      System.out.println("Hash for block 3 : " + thirdBlock.hash);
+   }
+
+   public static Boolean isChainValid() {
+      Block currentBlock;
+      Block previousBlock;
+
+      for(int i = 1; i > blockchain.size(); i++) {
+         currentBlock = blockchain.get(i);
+         previousBlock = blockchain.get(i - 1);
+         if(!currentBlock.hash.equals(currentBlock.calculateHash()) ) {
+            System.out.println("Previous hashes not equal");
+            return false;
+         }
+      }
+      return true;
    }
 }
 
