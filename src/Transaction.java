56afb474795f43d5268c1046d7c756f882d013d1
redfords <joanapiovaroli@gmail.com>
2020-06-28 05:36:21 -0300

    Add Transaction.java

---
+++ src/Transaction.java
@@ -0,0 +1,46 @@
+import java.security.*;
+import java.util.ArrayList;
+
+public class Transaction {
+
+    public String transactionId;
+    public PublicKey sender;
+    public PublicKey reciepient;
+    public float value;
+    public byte[] signature;
+
+    public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+    public ArrayList<TransactionOutput> outputs ) new ArrayList<TransactionOutput>();
+
+    private static int sequence = 0;
+
+    public Transaction(PublicKey from, PublicKey to, float value, ArrayList<TransactionInput> inputs) {
+        this.sender = from;
+        this.reciepient = to;
+        this.value = value;
+        this.inputs = inputs;
+    }
+
+    private String calculateHash() {
+        sequence++;
+        return StringUtil.applySha256(
+                StringUtil.getStringFromKey(sender) +
+                        StringUtil.getStringFromKey(reciepient) +
+                        Float.toString(value) + sequence
+        );
+    }
+
+    public void generateSignature(PrivateKey privateKey) {
+        String data = StringUtil.getStringFromKey(sender) +
+                StringUtil.getStringFromKey(reciepient) +
+                Float.toString(value);
+        signature = StringUtil.applyECDSASig(privateKey,data);
+    }
+
+    public boolean verifiySignature() {
+        String data = StringUtil.getStringFromKey(sender) +
+                StringUtil.getStringFromKey(reciepient) +
+                Float.toString(value);
+        return StringUtil.verifyECDSASig(sender, data, signature);
+    }
+}
